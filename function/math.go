cf51568af18a26b0c41100d8b33bc298945d0d2a
DESKTOP-ECNPOH2\dell <hgfgogogo@163.com>
2022-04-02 09:33:30 +0800

    modify

--- function/math.go
+++ function/math.go
@@ -7,3 +7,8 @@ func Round(val float64, num int32) (res float64, exact bool) {
 	res, exact = decimal.NewFromFloat(val).Round(num).Float64()
 	return
 }
+
+// Intercept 截取小数点 [retain为保留的小数点数]
+func Intercept(val float64, retain int32) (res float64) {
+	return
+}
