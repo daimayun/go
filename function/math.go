ca0e4d50f758b67ae061e5c6f4a95a8d5ea41ce4
DESKTOP-ECNPOH2\dell <hgfgogogo@163.com>
2022-04-07 18:07:15 +0800

    math

--- function/math.go
+++ function/math.go
@@ -1,6 +1,10 @@
 package function
 
-import "github.com/shopspring/decimal"
+import (
+	"fmt"
+	"github.com/shopspring/decimal"
+	"strconv"
+)
 
 // Round 四舍五入 [num为保留的小数点位数]
 func Round(val float64, num int32) (res float64, exact bool) {
@@ -10,5 +14,12 @@ func Round(val float64, num int32) (res float64, exact bool) {
 
 // Intercept 截取小数点 [retain为保留的小数点数]
 func Intercept(val float64, retain int32) (res float64) {
+	return
+}
+
+// FloatRound 四舍五入 [n为保留的小数点位数]
+func FloatRound(f float64, n int) (res float64, err error) {
+	format := "%." + strconv.Itoa(n) + "f"
+	res, err = strconv.ParseFloat(fmt.Sprintf(format, f), 64)
 	return
 }
