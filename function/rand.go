f84924074617d3f64d83a7b4109c700c5614128e
wangben <1403375030@qq.com>
2022-04-22 09:59:51 +0800

    rand

--- function/rand.go
+++ function/rand.go
@@ -1,7 +1,10 @@
 package function
 
 import (
+	cr "crypto/rand"
+	"encoding/base64"
 	"fmt"
+	"io"
 	"math/rand"
 	"strconv"
 	"time"
@@ -10,4 +13,13 @@ import (
 // RandString 32位随机字符串
 func RandString() string {
 	return Md5(fmt.Sprintf("%s%s", strconv.FormatInt(time.Now().UnixNano(), 10), strconv.Itoa(rand.Intn(1000000))))
+}
+
+// UniqueId 生成Guid字串
+func UniqueId() string {
+	b := make([]byte, 48)
+	if _, err := io.ReadFull(cr.Reader, b); err != nil {
+		return ""
+	}
+	return Md5(base64.URLEncoding.EncodeToString(b))
 }
