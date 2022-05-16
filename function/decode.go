7efe5babc853067661e98fdd6f3a9019e52e64a8
wangben <1403375030@qq.com>
2022-05-16 13:47:24 +0800

    encode

--- function/decode.go
+++ function/decode.go
@@ -1,9 +1,21 @@
 package function
 
-import Url "net/url"
+import (
+	"encoding/base64"
+	"net/url"
+)
 
 // UrlDecode url_decode
-func UrlDecode(str string) (url string) {
-	url, _ = Url.QueryUnescape(str)
-	return
+func UrlDecode(str string) string {
+	data, _ := url.QueryUnescape(str)
+	return data
+}
+
+// Base64Decode base64_decode
+func Base64Decode(str string) string {
+	data, err := base64.StdEncoding.DecodeString(str)
+	if err != nil {
+		return ""
+	}
+	return string(data)
 }
--- function/encode.go
+++ function/encode.go
@@ -1,10 +1,16 @@
 package function
 
 import (
-	Url "net/url"
+	"encoding/base64"
+	"net/url"
 )
 
 // UrlEncode url_encode
-func UrlEncode(url string) string {
-	return Url.QueryEscape(url)
+func UrlEncode(str string) string {
+	return url.QueryEscape(str)
+}
+
+// Base64Encode base64_encode
+func Base64Encode(str string) string {
+	return base64.StdEncoding.EncodeToString([]byte(str))
 }
