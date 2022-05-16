5fdacecee6dabcbd329d4159a66431d5592256ba
wangben <1403375030@qq.com>
2022-05-16 11:46:48 +0800

    file

--- function/file.go
+++ function/file.go
@@ -2,6 +2,7 @@ package function
 
 import (
 	"io/ioutil"
+	"os"
 )
 
 // FileGetContents 把整个文件读入一个字符串中
@@ -12,7 +13,19 @@ func FileGetContents(filename string) (str string, err error) {
 	return
 }
 
-// FilePutContents 把一个字符串写入文件中
+// FilePutContents 把一个字符串写入文件中[覆盖原文件内容]
 func FilePutContents(filename string, data string) error {
 	return ioutil.WriteFile(filename, []byte(data), 0644)
+}
+
+// FilePutContentByAppend 把一个字符串写入文件中[追加至原文件]
+func FilePutContentByAppend(filename string, data string) (err error) {
+	var f *os.File
+	f, err = os.OpenFile(filename, os.O_APPEND|os.O_CREATE, 0644)
+	if err != nil {
+		return
+	}
+	defer f.Close()
+	_, err = f.Write([]byte(data))
+	return
 }
