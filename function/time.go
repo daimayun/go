82e63d49f7a795d5fc0d27bafdfb78a1fd6e5b63
wangben <1403375030@qq.com>
2022-05-20 14:13:37 +0800

    time

--- function/time.go
+++ function/time.go
@@ -8,44 +8,64 @@ var (
 	TimeLayoutYMD    string = "2006-01-02"
 )
 
-// TodayDateTimeStart 今日00:00:00时间
-func TodayDateTimeStart() time.Time {
-	currentTime := time.Now()
-	return time.Date(currentTime.Year(), currentTime.Month(), currentTime.Day(), 0, 0, 0, 0, currentTime.Location())
+// 当前时间
+var now = time.Now()
+
+// TodayStartAndEndTime 今天00:00:00时间和今天23:59:59时间
+func TodayStartAndEndTime() (startTime, endTime time.Time) {
+	startTime = time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, now.Location())
+	endTime = time.Date(now.Year(), now.Month(), now.Day(), 23, 59, 59, 0, now.Location())
+	return
 }
 
-// TodayDateTimeEnd 今日23:59:59时间
-func TodayDateTimeEnd() time.Time {
-	currentTime := time.Now()
-	return time.Date(currentTime.Year(), currentTime.Month(), currentTime.Day(), 23, 59, 59, 0, currentTime.Location())
+// NowWeekStartAndEndTime 本周一00:00:00时间和本周日23:59:59时间
+func NowWeekStartAndEndTime() (startTime, endTime time.Time) {
+	offset := int(time.Monday - now.Weekday())
+	//周日做特殊判断 因为time.Monday = 0
+	if offset > 0 {
+		offset = -6
+	}
+	lastOffset := int(time.Saturday - now.Weekday())
+	//周日做特殊判断 因为time.Monday = 0
+	if lastOffset == 6 {
+		lastOffset = -1
+	}
+	startTime = time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, time.Local).AddDate(0, 0, offset)
+	endTime = time.Date(now.Year(), now.Month(), now.Day(), 23, 59, 59, 0, time.Local).AddDate(0, 0, lastOffset+1)
+	return
 }
 
-// NowMonthDateTimeStart 当前月1号00:00:00时间
-func NowMonthDateTimeStart() time.Time {
-	currentTime := time.Now()
-	return time.Date(currentTime.Year(), currentTime.Month(), 1, 0, 0, 0, 0, currentTime.Location())
+// NowMonthStartAndEndTime 本月1号00:00:00时间和本月末23:59:59时间
+func NowMonthStartAndEndTime() (startTime, endTime time.Time) {
+	startTime = time.Date(now.Year(), now.Month(), 1, 0, 0, 0, 0, now.Location())
+	endTime = time.Date(now.Year(), now.Month(), startTime.AddDate(0, 1, -1).Day(), 23, 59, 59, 0, now.Location())
+	return
 }
 
-// NowMonthDateTimeEnd 当前月最后一天的23:59:59时间
-func NowMonthDateTimeEnd() time.Time {
-	now := time.Now()
-	currentYear, currentMonth, _ := now.Date()
-	currentLocation := now.Location()
-	firstOfMonth := time.Date(currentYear, currentMonth, 1, 0, 0, 0, 0, currentLocation)
-	nowDate := firstOfMonth.AddDate(0, 1, -1)
-	return time.Date(currentYear, currentMonth, nowDate.Day(), 23, 59, 59, 0, currentLocation)
+// NowQuarterStartAndEndTime 本季度1号00:00:00时间和本季度末23:59:59时间
+func NowQuarterStartAndEndTime() (startTime, endTime time.Time) {
+	month := int(now.Month())
+	if month >= 1 && month <= 3 {
+		startTime = time.Date(now.Year(), 1, 1, 0, 0, 0, 0, time.Local)
+		endTime = time.Date(now.Year(), 3, 31, 23, 59, 59, 0, time.Local)
+	} else if month >= 4 && month <= 6 {
+		startTime = time.Date(now.Year(), 4, 1, 0, 0, 0, 0, time.Local)
+		endTime = time.Date(now.Year(), 6, 30, 23, 59, 59, 0, time.Local)
+	} else if month >= 7 && month <= 9 {
+		startTime = time.Date(now.Year(), 7, 1, 0, 0, 0, 0, time.Local)
+		endTime = time.Date(now.Year(), 9, 30, 23, 59, 59, 0, time.Local)
+	} else {
+		startTime = time.Date(now.Year(), 10, 1, 0, 0, 0, 0, time.Local)
+		endTime = time.Date(now.Year(), 12, 31, 23, 59, 59, 0, time.Local)
+	}
+	return
 }
 
-// NowYearDateTimeStart 当年1月1号00:00:00时间
-func NowYearDateTimeStart() time.Time {
-	currentTime := time.Now()
-	return time.Date(currentTime.Year(), 1, 1, 0, 0, 0, 0, currentTime.Location())
-}
-
-// NowYearDateTimeEnd 当年12月31号的23:59:59时间
-func NowYearDateTimeEnd() time.Time {
-	currentTime := time.Now()
-	return time.Date(currentTime.Year(), 12, 31, 23, 59, 59, 0, currentTime.Location())
+// NowYearStartAndEndTime 本年1月1号00:00:00时间和本年12月31号23:59:59时间
+func NowYearStartAndEndTime() (startTime, endTime time.Time) {
+	startTime = time.Date(now.Year(), 1, 1, 0, 0, 0, 0, now.Location())
+	endTime = time.Date(now.Year(), 12, 31, 23, 59, 59, 0, now.Location())
+	return
 }
 
 // BeforeSecondTime N秒前的时间
@@ -88,7 +108,7 @@ func AfterYearTime(year int) time.Time {
 	return time.Now().AddDate(year, 0, 0)
 }
 
-//StringToTime 将字符串转为时间（例如：2021-08-08 08:08:08）
+//StringToTime 将字符串转为时间[2021-08-08 08:08:08]
 func StringToTime(str string, layouts ...string) (time.Time, error) {
 	layout := TimeLayout
 	if len(layouts) > 0 {
@@ -97,7 +117,7 @@ func StringToTime(str string, layouts ...string) (time.Time, error) {
 	return time.Parse(layout, str)
 }
 
-// TimestampToTime 时间戳转日期
+// TimestampToTime 时间戳转时间
 func TimestampToTime(timestamp int64) time.Time {
 	return time.Unix(timestamp, 0)
 }
