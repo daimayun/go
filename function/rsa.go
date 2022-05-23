fbfb4c9b531b0ea65a2b9b1be257fb32473d4174
wangben <1403375030@qq.com>
2022-05-23 09:08:44 +0800

    rsa

--- function/rsa.go
+++ function/rsa.go
@@ -52,3 +52,43 @@ func RsaDecrypt(cipherByte []byte, privateKey string) (plainText string, err err
 	plainText = string(decryptOAEP)
 	return
 }
+
+// RsaEncryptByte 使用对方的公钥的数据,只有对方的私钥才能解开
+func RsaEncryptByte(plain []byte, publicKey string) (cipherByte []byte, err error) {
+	var (
+		pubKeyValue interface{}
+		encryptOAEP []byte
+	)
+	// 解码公钥
+	pubBlock, _ := pem.Decode([]byte(publicKey))
+	// 读取公钥
+	pubKeyValue, err = x509.ParsePKIXPublicKey(pubBlock.Bytes)
+	if err != nil {
+		return
+	}
+	pub := pubKeyValue.(*rsa.PublicKey)
+	// 加密数据方法:不用使用EncryptPKCS1v15方法加密,源码里面推荐使用EncryptOAEP,因此这里使用安全的方法加密
+	encryptOAEP, err = rsa.EncryptOAEP(sha1.New(), rand.Reader, pub, plain, nil)
+	if err != nil {
+		return
+	}
+	cipherByte = encryptOAEP
+	return
+}
+
+// RsaDecryptByte 使用私钥解密公钥加密的数据
+func RsaDecryptByte(cipherByte []byte, privateKey string) (decryptOAEP []byte, err error) {
+	var priKey *rsa.PrivateKey
+	// 解析出私钥
+	priBlock, _ := pem.Decode([]byte(privateKey))
+	priKey, err = x509.ParsePKCS1PrivateKey(priBlock.Bytes)
+	if err != nil {
+		return
+	}
+	// 解密RSA-OAEP方式加密后的内容
+	decryptOAEP, err = rsa.DecryptOAEP(sha1.New(), rand.Reader, priKey, cipherByte, nil)
+	if err != nil {
+		return
+	}
+	return
+}
