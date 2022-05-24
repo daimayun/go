ce95c3b2c5ed0b713bd933e62ad1ec3596267e48
wangben <1403375030@qq.com>
2022-05-24 15:07:52 +0800

    map

--- function/map.go
+++ function/map.go
@@ -1,12 +1,19 @@
 package function
 
 // MapMerge 两个MAP合并,两个MAP键相同的情况,后面MAP的值覆盖前面MAP的值
-func MapMerge(m1, m2 map[string]string) (m map[string]string) {
+func MapMerge(m1, m2 map[string]string, trimEmpties ...bool) (m map[string]string) {
+	trimEmpty := false
+	if len(trimEmpties) > 0 {
+		trimEmpty = trimEmpties[0]
+	}
 	m = make(map[string]string)
 	for i, v := range m1 {
 		for j, w := range m2 {
 			if i == j {
 				m[i] = w
+				if trimEmpty && w == "" {
+					m[i] = v
+				}
 			} else {
 				if _, ok := m[i]; !ok {
 					m[i] = v
