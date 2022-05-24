feacd9f7fbbda895803623f679e9c5cd3eb117b1
wangben <1403375030@qq.com>
2022-05-24 08:28:49 +0800

    base58

--- function/decode.go
+++ function/decode.go
@@ -1,7 +1,9 @@
 package function
 
 import (
+	"bytes"
 	"encoding/base64"
+	"math/big"
 	"net/url"
 )
 
@@ -18,4 +20,16 @@ func Base64Decode(str string) string {
 		return ""
 	}
 	return string(data)
+}
+
+// Base58Decode base58_decode
+func Base58Decode(str string) string {
+	strByte := []byte(str)
+	ret := big.NewInt(0)
+	for _, byteElem := range strByte {
+		index := bytes.IndexByte(base58, byteElem)
+		ret.Mul(ret, big.NewInt(58))
+		ret.Add(ret, big.NewInt(int64(index)))
+	}
+	return string(ret.Bytes())
 }
--- function/encode.go
+++ function/encode.go
@@ -2,6 +2,7 @@ package function
 
 import (
 	"encoding/base64"
+	"math/big"
 	"net/url"
 )
 
@@ -13,4 +14,40 @@ func UrlEncode(str string) string {
 // Base64Encode base64_encode
 func Base64Encode(str string) string {
 	return base64.StdEncoding.EncodeToString([]byte(str))
+}
+
+var base58 = []byte("123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz")
+
+// Base58Encode base58_encode
+func Base58Encode(str string) string {
+	//1. 转换成ascii码对应的值
+	strByte := []byte(str)
+	//2. 转换十进制
+	strTen := big.NewInt(0).SetBytes(strByte)
+	//3. 取出余数
+	var modSlice []byte
+	for strTen.Cmp(big.NewInt(0)) > 0 {
+		mod := big.NewInt(0) //余数
+		strTen58 := big.NewInt(58)
+		strTen.DivMod(strTen, strTen58, mod)             //取余运算
+		modSlice = append(modSlice, base58[mod.Int64()]) //存储余数,并将对应值放入其中
+	}
+	//  处理0就是1的情况 0使用字节'1'代替
+	for _, elem := range strByte {
+		if elem != 0 {
+			break
+		} else if elem == 0 {
+			modSlice = append(modSlice, byte('1'))
+		}
+	}
+	ReverseModSlice := reverseByteArr(modSlice)
+	return string(ReverseModSlice)
+}
+
+// reverseByteArr 将字节的数组反转
+func reverseByteArr(bytes []byte) []byte {
+	for i := 0; i < len(bytes)/2; i++ {
+		bytes[i], bytes[len(bytes)-1-i] = bytes[len(bytes)-1-i], bytes[i] //前后交换
+	}
+	return bytes
 }
