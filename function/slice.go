74320844e0dfefecfd50305d00e81425ba9e9908
wangben <1403375030@qq.com>
2022-06-22 10:43:45 +0800

    slice

--- function/slice.go
+++ function/slice.go
@@ -9,3 +9,22 @@ func InSlice(slice []string, target string) bool {
 	}
 	return false
 }
+
+// SliceUnique 切片去重
+func SliceUnique(slice []string) []string {
+	if len(slice) <= 1 {
+		return slice
+	}
+	var (
+		newSlice = make([]string, 0)
+		m        = make(map[string]interface{})
+	)
+	for _, v := range slice {
+		if _, ok := m[v]; ok {
+			continue
+		}
+		m[v] = 0
+		newSlice = append(newSlice, v)
+	}
+	return newSlice
+}
