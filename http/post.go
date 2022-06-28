2627911608d53b0c15b3997a303f41678c8d3a05
wangben <1403375030@qq.com>
2022-06-28 11:21:14 +0800

    http

--- http/post.go
+++ http/post.go
@@ -7,8 +7,8 @@ import (
 	"net/url"
 )
 
-// PostJson post_json
-func PostJson(url string, jsonByteBuffer *bytes.Buffer, headers ...map[string]string) (b []byte, err error) {
+// postJson post_json
+func postJson(url string, jsonByteBuffer *bytes.Buffer, headers ...map[string]string) (b []byte, err error) {
 	if len(headers) > 0 {
 		if _, ok := headers[0]["Content-Type"]; !ok {
 			headers[0]["Content-Type"] = "application/json"
@@ -17,6 +17,26 @@ func PostJson(url string, jsonByteBuffer *bytes.Buffer, headers ...map[string]st
 		headers = []map[string]string{{"Content-Type": "application/json"}}
 	}
 	return Request(nh.MethodPost, url, jsonByteBuffer, headers...)
+}
+
+// PostJson post_json
+func PostJson(url string, jsonByte []byte) (b []byte, err error) {
+	var (
+		req *nh.Request
+		res *nh.Response
+	)
+	req, err = nh.NewRequest(nh.MethodPost, url, bytes.NewBuffer(jsonByte))
+	if err != nil {
+		return
+	}
+	req.Header.Set("Content-Type", "application/json")
+	res, err = nh.DefaultClient.Do(req)
+	if err != nil {
+		return
+	}
+	defer res.Body.Close()
+	b, err = ioutil.ReadAll(res.Body)
+	return
 }
 
 // PostForm post_form
