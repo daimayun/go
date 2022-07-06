846dd4a0ef2f171fbf2747bfd442c77a33157e5e
wangben <1403375030@qq.com>
2022-07-06 16:16:39 +0800

    sms

--- aliyun/sms/client.go
+++ aliyun/sms/client.go
@@ -1,12 +1,69 @@
 package sms
 
-import "github.com/aliyun/alibaba-cloud-sdk-go/services/dysmsapi"
+import (
+	"errors"
+	openapi "github.com/alibabacloud-go/darabonba-openapi/client"
+	dysmsapi20170525 "github.com/alibabacloud-go/dysmsapi-20170525/v2/client"
+	util "github.com/alibabacloud-go/tea-utils/service"
+	"github.com/alibabacloud-go/tea/tea"
+)
 
-func Client(regionId, accessKeyId, accessKeySecret string) (client *dysmsapi.Client, err error) {
-	client, err = dysmsapi.NewClientWithAccessKey(regionId, accessKeyId, accessKeySecret)
+const endpoint = "dysmsapi.aliyuncs.com"
+
+// RequestSendSmsData 请求发送短信数据
+type RequestSendSmsData struct {
+	AccessKeyId     string `json:"access_key_id"`
+	AccessKeySecret string `json:"access_key_secret"`
+	RegionId        string `json:"region_id,omitempty"`
+	PhoneNumbers    string `json:"phone_numbers"`
+	SignName        string `json:"sign_name"`
+	TemplateCode    string `json:"template_code"`
+	TemplateParam   string `json:"template_param"`
+}
+
+// CreateClient 创建Client
+func CreateClient(accessKeyId, accessKeySecret string) (result *dysmsapi20170525.Client, err error) {
+	config := &openapi.Config{
+		AccessKeyId:     tea.String(accessKeyId),
+		AccessKeySecret: tea.String(accessKeySecret),
+		Endpoint:        tea.String(endpoint),
+	}
+	result = &dysmsapi20170525.Client{}
+	result, err = dysmsapi20170525.NewClient(config)
 	return
 }
 
-func SendSms() {
-	//
+// SendSms 发送短信
+func SendSms(data RequestSendSmsData) (err error) {
+	var client *dysmsapi20170525.Client
+	client, err = CreateClient(data.AccessKeyId, data.AccessKeySecret)
+	if err != nil {
+		return
+	}
+	sendSmsRequest := &dysmsapi20170525.SendSmsRequest{
+		PhoneNumbers:  tea.String(data.PhoneNumbers),
+		SignName:      tea.String(data.SignName),
+		TemplateCode:  tea.String(data.TemplateCode),
+		TemplateParam: tea.String(data.TemplateParam),
+	}
+	runtime := &util.RuntimeOptions{}
+	catchError := func() (e error) {
+		defer func() {
+			if r := tea.Recover(recover()); r != nil {
+				e = r
+			}
+		}()
+		_, e = client.SendSmsWithOptions(sendSmsRequest, runtime)
+		return
+	}()
+	if catchError != nil {
+		var e = &tea.SDKError{}
+		if t, ok := catchError.(*tea.SDKError); ok {
+			e = t
+		} else {
+			e.Message = tea.String(catchError.Error())
+		}
+		err = errors.New("短信发送失败: " + err.Error())
+	}
+	return
 }
--- go.mod
+++ go.mod
@@ -3,19 +3,25 @@ module github.com/daimayun/go
 go 1.16
 
 require (
-	github.com/aliyun/alibaba-cloud-sdk-go v1.61.1641
+	github.com/alibabacloud-go/darabonba-openapi v0.1.18
+	github.com/alibabacloud-go/dysmsapi-20170525/v2 v2.0.14
+	github.com/alibabacloud-go/tea v1.1.17
+	github.com/alibabacloud-go/tea-utils v1.4.5
 	github.com/boombuler/barcode v1.0.1
 	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0
 	github.com/google/go-querystring v1.1.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/mojocn/base64Captcha v1.3.5
 	github.com/mozillazg/go-httpheader v0.3.0 // indirect
 	github.com/mozillazg/go-pinyin v0.19.0
 	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646
 	github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e
+	github.com/stretchr/testify v1.7.0 // indirect
 	github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/common v1.0.433
 	github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/ocr v1.0.433
 	github.com/tencentyun/cos-go-sdk-v5 v0.7.28
 	golang.org/x/crypto v0.0.0-20220411220226-7b82a4e95df4
 	gopkg.in/ini.v1 v1.66.6 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
