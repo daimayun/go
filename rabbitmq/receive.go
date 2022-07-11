365649f733083e4febf6d9bb640fcce96a5aa4e5
wangben <1403375030@qq.com>
2022-07-11 13:45:54 +0800

    rabbitmq

--- rabbitmq/receive.go
+++ rabbitmq/receive.go
@@ -6,20 +6,19 @@ import (
 
 // ReceiveData 接收处理队列所提交数据
 type ReceiveData struct {
-	Exchange         string     `json:"exchange"`
-	Type             string     `json:"type"`
-	Durable          bool       `json:"durable"`
-	AutoDelete       bool       `json:"auto_delete"`
-	Internal         bool       `json:"internal"`
-	NoWait           bool       `json:"no_wait"`
-	Args             amqp.Table `json:"args"`
-	DeleteWhenUnused bool       `json:"delete_when_unused"`
-	Exclusive        bool       `json:"exclusive"`
-	QueueName        string     `json:"queue_name"`
-	RoutingKey       string     `json:"routing_key"`
-	Consumer         string     `json:"consumer"`
-	AutoAck          bool       `json:"auto_ack"`
-	NoLocal          bool       `json:"no_local"`
+	Exchange   string     `json:"exchange"`
+	Type       string     `json:"type"`
+	Durable    bool       `json:"durable"`
+	AutoDelete bool       `json:"auto_delete"`
+	Internal   bool       `json:"internal"`
+	NoWait     bool       `json:"no_wait"`
+	Args       amqp.Table `json:"args"`
+	Exclusive  bool       `json:"exclusive"`
+	QueueName  string     `json:"queue_name"`
+	RoutingKey string     `json:"routing_key"`
+	Consumer   string     `json:"consumer"`
+	AutoAck    bool       `json:"auto_ack"`
+	NoLocal    bool       `json:"no_local"`
 }
 
 // Receive 接收消息队列
@@ -39,7 +38,7 @@ func (conn Connection) Receive(data ReceiveData) (messages <-chan amqp.Delivery,
 			return
 		}
 	}
-	q, err = ch.QueueDeclare(data.QueueName, data.Durable, data.DeleteWhenUnused, data.Exclusive, data.NoWait, data.Args)
+	q, err = ch.QueueDeclare(data.QueueName, data.Durable, data.AutoDelete, data.Exclusive, data.NoWait, data.Args)
 	if err != nil {
 		return
 	}
