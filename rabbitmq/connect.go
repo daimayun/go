e00a59c4c196e7d0e46ffb880f7235cb5971df13
wangben <1403375030@qq.com>
2022-07-13 16:30:00 +0800

    rabbitmq

--- rabbitmq/connect.go
+++ rabbitmq/connect.go
@@ -2,6 +2,12 @@ package rabbitmq
 
 import amqp "github.com/rabbitmq/amqp091-go"
 
+// Queue 队列接口
+type Queue interface {
+	Send(data SendData) (err error)
+	Receive(data ReceiveData) (messages <-chan amqp.Delivery, ch *amqp.Channel, err error)
+}
+
 // Connection RabbitMQ连接
 type Connection struct {
 	Conn *amqp.Connection
@@ -12,7 +18,7 @@ func connection(url string) (conn *amqp.Connection, err error) {
 	return amqp.Dial(url)
 }
 
-// NewConnection RabbitMQ连接
+// NewConnection 实例化RabbitMQ连接
 func NewConnection(url string) (conn Connection, err error) {
 	var c *amqp.Connection
 	c, err = connection(url)
