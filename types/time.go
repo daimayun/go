47a624685e5c3f74aabf2ad3f8c046fe22876196
wangben <wangben-1203@qq.com>
2023-06-30 09:33:17 +0800

    unmarshal

--- types/time.go
+++ types/time.go
@@ -1,6 +1,9 @@
 package types
 
-import "time"
+import (
+	"bytes"
+	"time"
+)
 
 type Time time.Time
 
@@ -10,4 +13,13 @@ func (t Time) String() string {
 
 func (t Time) MarshalJSON() ([]byte, error) {
 	return []byte(`"` + time.Time(t).In(time.Local).Format("2006-01-02 15:04:05") + `"`), nil
+}
+
+func (t *Time) UnmarshalJSON(b []byte) (err error) {
+	var tm time.Time
+	if tm, err = time.ParseInLocation("2006-01-02 15:04:05", string(bytes.Trim(b, "\"")), time.Local); err != nil {
+		return
+	}
+	*t = Time(tm)
+	return nil
 }
